b4573cb0bb83cdf70476a3983e8eef364d4f0677
jy95 <jacques.yakoub@gmail.com>
2017-09-09 19:10:26 +0200

    :tada: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules" :{
+        "linebreak-style": 0,
+        "valid-jsdoc": "error",
+        "max-len": ["error", { "ignoreComments": true }],
+        "import/extensions": ['off', 'never'],
+        "no-bitwise": ["error", { "allow": ["|"] }],
+        "no-restricted-syntax": ['off', "ForInStatement"],
+        "prefer-const" : "off"
+    },
+    "env" : {
+        "es6" : true,
+        "node": true
+    }
+};
